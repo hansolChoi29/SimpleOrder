6c9440fc484d1795d159281bb2ab3229e5531a55
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-06-15 11:50:41 +0900

    docs: Swagger UI add

--- src/main/java/com/sparta/simpleorder/domain/orders/controller/OrderController.java
+++ src/main/java/com/sparta/simpleorder/domain/orders/controller/OrderController.java
@@ -8,6 +8,8 @@ import com.sparta.simpleorder.domain.orders.dto.response.GetOneResponseDto;
 import com.sparta.simpleorder.domain.orders.dto.response.UpdateResponseDto;
 import com.sparta.simpleorder.domain.orders.service.OrderService;
 import com.sparta.simpleorder.domain.orders.dto.request.UpdateRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,12 +19,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@Tag(name = "Orders", description = "주문 등록·조회·수정·삭제 API")
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
 
+    @Operation(summary = "주문 생성", description = "상품 ID와 주문 수량을 입력하여 주문을 생성합니다.")
     @PostMapping
     public ResponseEntity<CreateResponseDto> create(
             @RequestBody CreateRequestDto request
@@ -33,6 +37,7 @@ public class OrderController {
                 .body(response);
     }
 
+    @Operation(summary = "주문 단건조회", description = "주문 ID로 특정 주문의 상세 정보를 조회합니다.")
     @GetMapping("/{id}")
     public ResponseEntity<GetOneResponseDto> getOne(
             @PathVariable("id") Long id
@@ -41,6 +46,7 @@ public class OrderController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "주문 전체조회", description = "page와 size를 입력하여 페이지 기반으로 주문 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<Page<GetListResponseDto>> getList(
         @RequestParam(defaultValue = "0") int page,
@@ -51,6 +57,7 @@ public class OrderController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "주문 수정", description = "주문 ID로 대상을 지정하고, 주문 수량·상태를 수정합니다.")
     @PatchMapping("/{id}")
     public ResponseEntity<UpdateResponseDto> update(
             @RequestBody UpdateRequestDto request,
@@ -60,6 +67,7 @@ public class OrderController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "주문 삭제", description = "주문 ID로 대상을 지정하여 삭제합니다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(
         @PathVariable("id") Long id
--- src/main/java/com/sparta/simpleorder/domain/products/controller/ProductController.java
+++ src/main/java/com/sparta/simpleorder/domain/products/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.sparta.simpleorder.domain.products.dto.response.GetListResponseDto;
 import com.sparta.simpleorder.domain.products.dto.response.GetOneResponseDto;
 import com.sparta.simpleorder.domain.products.dto.response.UpdateResponseDto;
 import com.sparta.simpleorder.domain.products.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,16 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@Tag(name = "Products", description = "상품 등록·조회·수정·삭제 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
 
+
+    @Operation(summary = "상품 생성", description = "상품명, 가격, 수량을 입력하여 생성합니다")
     @PostMapping
     public ResponseEntity<CreateResponseDto> create(
             @RequestBody CreateRequestDto request
@@ -30,6 +36,7 @@ public class ProductController {
                 .body(response);
     }
 
+    @Operation(summary = "상품 단건조회", description = "상품 ID로 특정 상품의 상세 정보를 조회합니다.")
     @GetMapping("/{id}")
     public ResponseEntity<GetOneResponseDto> getOne(
             @PathVariable("id") Long id
@@ -38,12 +45,14 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "상품 전체조회", description = "등록된 모든 상품을 조회합니다.")
     @GetMapping
     public ResponseEntity<List<GetListResponseDto>> getList(){
         List<GetListResponseDto> response = productService.getList();
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "상품 수정", description = "상품 ID로 대상을 지정하고, 상품명·가격·재고 수량·상태를 수정합니다.")
     @PatchMapping("/{id}")
     public ResponseEntity<UpdateResponseDto> update(
             @RequestBody UpdateRequestDto request,
@@ -53,6 +62,7 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "상품 삭제", description = "상품 ID로 대상을 지정하여 삭제합니다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(
             @PathVariable("id") Long id
